97cab1924d1dbb36b3af2e5bd20703c1da3b5ebb
Beat <beaterftr@gmail.com>
2025-08-01 15:33:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section>
+      <h1 className='bg-blue-500 text-3xl font-bold underline'>My First React App on Vite</h1>
+    </section>
+
+  )
+}
+
+export default App
